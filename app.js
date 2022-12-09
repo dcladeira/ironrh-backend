5572467d93fec6955cab7c555471e0543b4631e0
tatialveso <tatiialveso@gmail.com>
2022-12-09 10:24:12 -0300

    :ok_hand: format documents

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import dbConnect from './config/db.config.js'
-import userRouter from './routes/user.routes.js'
-import todoRouter from './routes/todo.routes.js'
-import uploadImgRouter from './routes/uploadImg.routes.js'
-import cors from 'cors'
+import express from "express";
+import * as dotenv from "dotenv";
+import dbConnect from "./config/db.config.js";
+import userRouter from "./routes/user.routes.js";
+import todoRouter from "./routes/todo.routes.js";
+import uploadImgRouter from "./routes/uploadImg.routes.js";
+import cors from "cors";
 
-dotenv.config()
+dotenv.config();
 
-dbConnect()
+dbConnect();
 
-const app = express()
+const app = express();
 
-app.use(cors( { origin: process.env.REACT_URL } ))
-app.use(express.json())
+app.use(cors({ origin: process.env.REACT_URL }));
+app.use(express.json());
 
-app.use('/user', userRouter)
-app.use('/todo', todoRouter)
-app.use('/', uploadImgRouter)
+app.use("/user", userRouter);
+app.use("/todo", todoRouter);
+app.use("/", uploadImgRouter);
 
-app.listen(Number(process.env.PORT),
-    () => console.log(`server on port ${process.env.PORT}!'`))
+app.listen(Number(process.env.PORT), () =>
+  console.log(`server on port ${process.env.PORT}!'`)
+);
--- models/user.model.js
+++ models/user.model.js
@@ -1,73 +1,74 @@
-import { model, Schema } from "mongoose"
+import { model, Schema } from "mongoose";
 
 const userSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-            match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/gm
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        profileImg: {
-            type: String,
-            default: "https://res.cloudinary.com/die2rduds/image/upload/v1670509301/ironrh-gallery/profile-picture_lbhtd4.png"
-        },
-        isAdmin: {
-            type: Boolean,
-            default: false
-        },
-        phone: {
-            type: String
-        },
-        department: {
-            type: String,
-        },
-        salary: {
-            type: Number
-        },
-        status: {
-            type: String,
-            enum: ["Disponível", "Alocado", "De Férias", "De Licença", "Produto"]
-        },
-        birthDate: {
-            type: Date,
-        },
-        admissionDate: {
-            type: Date
-        },
-        resignationDate: {
-            type: Date
-        },
-        active: {
-            type: Boolean,
-            default: true
-        },
-        address: {
-            city: { type: String },
-            state: { type: String }
-        },
-        todos: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "Todo"
-            }
-        ]
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-        timestamps: true,
-    }
-)
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/gm,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImg: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/die2rduds/image/upload/v1670509301/ironrh-gallery/profile-picture_lbhtd4.png",
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    phone: {
+      type: String,
+    },
+    department: {
+      type: String,
+    },
+    salary: {
+      type: Number,
+    },
+    status: {
+      type: String,
+      enum: ["Disponível", "Alocado", "De Férias", "De Licença", "Produto"],
+    },
+    birthDate: {
+      type: Date,
+    },
+    admissionDate: {
+      type: Date,
+    },
+    resignationDate: {
+      type: Date,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    address: {
+      city: { type: String },
+      state: { type: String },
+    },
+    todos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Todo",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const UserModel = model("User", userSchema)
+const UserModel = model("User", userSchema);
 
-export default UserModel
+export default UserModel;
