804cd58b27862fc6e979fa6254dd8c26d7f00f1d
tatialveso <tatiialveso@gmail.com>
2022-12-13 14:18:26 -0300

    :bookmark: create swagger documentation

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ import userRouter from "./routes/user.routes.js";
 import todoRouter from "./routes/todo.routes.js";
 import uploadImgRouter from "./routes/uploadImg.routes.js";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import bodyParser from "body-parser";
+import swaggerFile from './swagger/swagger_output.json' assert { type: "json"}
 
 dotenv.config();
 
@@ -18,6 +21,9 @@ app.use(express.json());
 app.use("/user", userRouter);
 app.use("/todo", todoRouter);
 app.use("/", uploadImgRouter);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(Number(process.env.PORT), () =>
   console.log(`server on port ${process.env.PORT}!'`)
